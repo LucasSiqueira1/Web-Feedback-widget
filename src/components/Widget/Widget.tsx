9514ff43c540dad83b6eaa19cc29f16e1d09b898
LucasSiqueira1 <lucas_siqueira28@outlook.com>
2022-05-03 14:13:56 -0300

    feat: acessilidade e estilo global

--- src/components/Widget/Widget.tsx
+++ src/components/Widget/Widget.tsx
@@ -1,26 +1,27 @@
 import React, { useState } from 'react';
 import { ChatTeardropDots } from "phosphor-react";
+import { Popover } from '@headlessui/react';
 
 export const Widget = () => {
-    const [isWidgetOpen, setIsWidgetOpen] = useState(false);
+    // const [isWidgetOpen, setIsWidgetOpen] = useState(false);
 
-    const visibleWidget = () => {
-        setIsWidgetOpen(!isWidgetOpen);
-    }
+    // const visibleWidget = () => {
+    //     setIsWidgetOpen(!isWidgetOpen);
+    // }
     return (
         <>
-            <div className="absolute bottom-5 right-5 ">
-                {isWidgetOpen && <p>Hello world</p>}
+            <Popover className="absolute bottom-5 right-5 ">
+                <Popover.Panel>Hello world</Popover.Panel>
 
-                <button onClick={() => visibleWidget()} className="bg-brand-500 text-white rounded-full px-3 h-12 flex items-center group">
+                <Popover.Button  className="bg-brand-500 text-white rounded-full px-3 h-12 flex items-center group">
                     <ChatTeardropDots className="w-6 h-6 " />
 
                     <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
                         <span className="pl-2"> </span>
                         Feedback
                     </span>
-                </button>
-            </div>
+                </Popover.Button>
+            </Popover>
         </>
     )
 }
