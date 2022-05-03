cdab02d239a82c857983d4db3a02d137fda5d066
LucasSiqueira1 <lucas_siqueira28@outlook.com>
2022-05-03 15:02:39 -0300

    feat: modal do widget form

--- src/components/WidgetForm/WidgetForm.tsx
+++ src/components/WidgetForm/WidgetForm.tsx
@@ -7,25 +7,25 @@ import ideaImage from '../../assets/idea.svg'
 import thoughtImage from '../../assets/thought.svg'
 
 
-const feedbackTypes ={
+const feedbackTypes = {
     BUG: {
         title: 'Problema',
         image: {
-            source: bugImage ,
+            source: bugImage,
             alt: 'Imagem de um inseto',
         },
     },
     IDEA: {
         title: 'Ideia',
         image: {
-            source: ideaImage ,
+            source: ideaImage,
             alt: 'Imagem de uma lampada',
         },
     },
     ORDER: {
         title: 'Outro',
         image: {
-            source: thoughtImage ,
+            source: thoughtImage,
             alt: 'Imagem de uma nuvem',
         },
     },
@@ -37,21 +37,26 @@ export const WidgetForm = () => {
             <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
                 <header>
                     <span className="text-xl leading-6">Deixe seu feedback</span>
-                    <CloseButton/>
+                    <CloseButton />
                 </header>
-                
+
                 <div className="flex py-8 gap-2 w-full">
                     {Object.entries(feedbackTypes).map(([key, value]) => {
-                        return(
-                            <button>
+                        return (
+                            <button
+                                key={value.title}
+                                className="bg-zinc-800 rounder-lg py-5 w-24 flex-1 flex-col items-center gap-2 border-2 border-transparent  hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                                type="button"
+
+                            >
                                 <img src={value.image.source} alt={value.image.alt} />
-                                <span>{value.title}</span> 
+                                <span>{value.title}</span>
                             </button>
                         )
                     })}
                 </div>
-                
-                
+
+
             </div>
         </>
     )
