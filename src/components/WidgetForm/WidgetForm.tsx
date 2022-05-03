9cdb8fe7fe2f4a54cbf0c17ed1fe4061740faac8
LucasSiqueira1 <lucas_siqueira28@outlook.com>
2022-05-03 14:51:16 -0300

    feat: objeto das imagens

--- src/components/WidgetForm/WidgetForm.tsx
+++ src/components/WidgetForm/WidgetForm.tsx
@@ -1,15 +1,51 @@
 import React, { useState } from 'react';
+import { CloseButton } from '../CloseButton/CloseButton';
+
+
+import bugImage from '../../assets/bug.svg'
+import ideaImage from '../../assets/idea.svg'
+import thoughtImage from '../../assets/thought.svg'
+
+
+const feedbackTypes ={
+    BUG: {
+        title: 'Problema',
+        image: {
+            source: bugImage ,
+            alt: 'Imagem de um inseto',
+        },
+    },
+    IDEA: {
+        title: 'Ideia',
+        image: {
+            source: ideaImage ,
+            alt: 'Imagem de uma lampada',
+        },
+    },
+    ORDER: {
+        title: 'Outro',
+        image: {
+            source: thoughtImage ,
+            alt: 'Imagem de uma nuvem',
+        },
+    },
+}
+
 export const WidgetForm = () => {
     return (
         <>
             <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
                 <header>
                     <span className="text-xl leading-6">Deixe seu feedback</span>
+                    <CloseButton/>
                 </header>
-                <h1>Hello world </h1>
+                
+                <div className="flex py-8 gap-2 w-full">
+                
+                </div>
                 
                 
             </div>
         </>
     )
-}
+}   
