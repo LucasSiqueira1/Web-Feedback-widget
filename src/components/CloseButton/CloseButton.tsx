46e578e0c806d22e728fdd5edb7da15e117c713c
LucasSiqueira1 <lucas_siqueira28@outlook.com>
2022-05-03 14:34:47 -0300

    feat: CloseButton

---
+++ src/components/CloseButton/CloseButton.tsx
@@ -0,0 +1,7 @@
+export const CloseButton = () => {
+    return(
+        <>
+            CloseButton
+        </>
+    )
+}
--- src/components/WidgetForm/WidgetForm.tsx
+++ src/components/WidgetForm/WidgetForm.tsx
@@ -3,7 +3,12 @@ export const WidgetForm = () => {
     return (
         <>
             <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-                Hello world
+                <header>
+                    <span className="text-xl leading-6">Deixe seu feedback</span>
+                </header>
+                <h1>Hello world </h1>
+                
+                
             </div>
         </>
     )
