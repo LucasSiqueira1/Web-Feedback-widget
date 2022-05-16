a798ced12d5f78018a09d714820ee400fbd609dd
LucasSiqueira1 <lucas_siqueira28@outlook.com>
2022-05-16 08:58:36 -0300

    loading no botão de enviar feedback

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,10 +1,11 @@
-import React, {FormEvent} from 'react';
+import React, { FormEvent } from 'react';
 import { feedbackTypes, FeedbackType } from ".."
 import { CloseButton } from "../../CloseButton"
 import { ArrowLeft, Camera } from "phosphor-react";
 import { ScreenshotButton } from "../ScreenshotButton";
 import { useState } from "react";
 import { api } from '../../../utils/api';
+import { Loading } from '../../Loading/Loading';
 
 interface FeedbackContentStepProps {
     props: FeedbackType
@@ -14,12 +15,14 @@ interface FeedbackContentStepProps {
 export const FeedbackContentStep = ({ props, propsFeedbackBack, propsFeedbackSend }: FeedbackContentStepProps) => {
     const [screenshot, setScreenshot] = useState<string | null>(null)
     const [comment, setComment] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
 
     const feedbackTypeInfo = feedbackTypes[props]
 
 
-    const handleSubmitFeedback = async(event: FormEvent) => {
+    const handleSubmitFeedback = async (event: FormEvent) => {
         event.preventDefault();
+        setIsLoading(true);
         // console.log(screenshot, comment)
 
         await api.post('/feedbacks', {
@@ -28,6 +31,7 @@ export const FeedbackContentStep = ({ props, propsFeedbackBack, propsFeedbackSen
             screenshot
         })
 
+        setIsLoading(true);
         propsFeedbackSend()
     }
 
@@ -55,10 +59,10 @@ export const FeedbackContentStep = ({ props, propsFeedbackBack, propsFeedbackSen
                     placeholder="Conte com detalhes o que está acontecendo..." />
 
                 <footer className="flex gap-2 mt-2">
-                    <ScreenshotButton props = {setScreenshot} screenshot ={screenshot}/>
-                    
-                    <button type="submit" disabled={comment.length === 0} className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500">
-                        Enviar feedback
+                    <ScreenshotButton props={setScreenshot} screenshot={screenshot} />
+
+                    <button type="submit" disabled={comment.length === 0 || isLoading} className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500">
+                        {isLoading ? <Loading/> : 'Enviar feedback'}
                     </button>
                 </footer>
             </form>
